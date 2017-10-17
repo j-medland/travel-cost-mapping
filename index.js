cf1ee944232a5188810824ca99fba3753aff9b78
John Medland <john.medland@manchester.ac.uk>
2017-10-17 15:23:29 +0100

    Fixed no. of requests bug

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ let stations = JSON.parse(fs.readFileSync('station-codes.json', 'utf-8'))
 let numberOfStations = stations.length
 
 // artificially clip number of stations for testing
-numberOfStations = 2
+numberOfStations = 6
 
 // run code
 getPrices()
 
 async function getPrices() {
     // determine how many requests will be made
-    const numberOfReq = Math.pow(2, numberOfStations - 1) - 1
+    const numberOfReq = (Math.pow(numberOfStations, 2) - numberOfStations) / 2
 
     console.log(`============== Total number of routes to search: ${numberOfReq} ==============`)
 
@@ -33,35 +33,33 @@ async function getPrices() {
         // iterate through routes and fire off price requests
     for (let fromIndex = 0; fromIndex < numberOfStations; fromIndex++) {
         for (let toIndex = fromIndex + 1; toIndex < numberOfStations; toIndex++) {
-            if (fromIndex != toIndex) {
-                // increment count
-                count++
-                const route = { from: stations[fromIndex].code, to: stations[toIndex].code }
-                    // create a page
-                const page = await browser.newPage();
-                // build url
-                url = `https://ojp.nationalrail.co.uk/service/timesandfares/${route.from}/${route.to}/today/${getSuitableTime()}/dep`
-                console.log(`Requesting (${count} of ${numberOfReq}) ${url}`)
-                    // try to request page and get price
-                try {
-                    await page.goto(url, { waitUntil: 'networkidle' })
-                    const price = await page.$eval('label[for^=faresingleFareOutward]', element => element.innerHTML.match(/£([0-9\.]*)/)[1])
-                    route.price = price
-                    console.log(`£${price}`)
-                        // write out route
-                    wstream.write(sep + JSON.stringify(route))
-                    if (!sep) {
-                        sep = ','
-                    }
-                } catch (e) {
-                    // something didn't work but we don't want to stop
-                    console.log(e)
+            // increment count
+            count++
+            const route = { from: stations[fromIndex].code, to: stations[toIndex].code }
+                // create a page
+            const page = await browser.newPage();
+            // build url
+            url = `https://ojp.nationalrail.co.uk/service/timesandfares/${route.from}/${route.to}/today/${getSuitableTime()}/dep`
+            console.log(`Requesting (${count} of ${numberOfReq}) ${url}`)
+                // try to request page and get price
+            try {
+                await page.goto(url, { waitUntil: 'networkidle' })
+                const price = await page.$eval('label[for^=faresingleFareOutward]', element => element.innerHTML.match(/£([0-9\.]*)/)[1])
+                route.price = price
+                console.log(`£${price}`)
+                    // write out route
+                wstream.write(sep + JSON.stringify(route))
+                if (!sep) {
+                    sep = ','
                 }
-                page.close().catch(err => { /*ignore error*/ })
-                    // sleep to avoid dos attack
-                if (count != numberOfReq) {
-                    await new Promise(resolve => setTimeout(resolve, sleep))
-                }
+            } catch (e) {
+                // something didn't work but we don't want to stop
+                console.log(e)
+            }
+            page.close().catch(err => { /*ignore error*/ })
+                // sleep to avoid dos attack
+            if (count != numberOfReq) {
+                await new Promise(resolve => setTimeout(resolve, sleep))
             }
         }
     }
